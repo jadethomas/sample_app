601d1e94568bc4cdb8e75100eedf7ad8394a1d14
jade Thomas <jade@wmd.co.nz>
2011-07-02 09:10:24 +1200

    Added a title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  
+  
+  # Return a title on a per-page basic
+  def title
+    base_title = "Ruby on Rials Tutorial Sample App"
+    
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
 end
