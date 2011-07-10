cb8dc4c8d95fcc237b28fdbbbaea5c78d1b3675f
jade Thomas <jade@wmd.co.nz>
2011-07-10 14:16:07 +1200

    Removed default Rails page

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -42,4 +42,16 @@ describe PagesController do
       end
   end
   
+  describe "Get" "Help" do
+    it "should be successful" do
+      get 'help' 
+      response.should be_success
+    end
+     it  "should have the right title" do
+        get 'help'
+        response.should have_selector("title" , 
+                        :content => "Ruby on Rials Tutorial Sample App | Help")
+      end
+  end
+  
 end
